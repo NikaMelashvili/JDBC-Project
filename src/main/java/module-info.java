3c651b8e025f75b0e269f804f2fb3864204319c5
Nika Melashvili <109832550+NikaMelashvili@users.noreply.github.com>
2024-01-25 00:17:18 +0400

    Delete src directory
    accidental

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.jdbcmysqlfull1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.jdbcmysqlfull1 to javafx.fxml;
-    exports com.example.jdbcmysqlfull1;
-}
